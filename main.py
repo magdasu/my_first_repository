4a8984b054d373ab767f16d35f7c6e0eff07e4fe
magdasu <102736967+magdasu@users.noreply.github.com>
2022-04-04 12:04:12 +0200

    dodano godzine

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import time
+
 def aa():
     a = input("Wpisz swoje imie: ")
     print(f"Moje pierwsze repo! {a}")
     print("Siema!")
+
+def godzina():
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t")
+    print("Jest godzina ", current_time)
 aa()
+godzina()
