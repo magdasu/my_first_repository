1ed99258cd4dc3e21c8d6331204d6a21b5a9b644
InsaneJraff <102656756+InsaneJraff@users.noreply.github.com>
2022-04-04 19:08:48 +0200

    dodana data dzisiejsza do main i nowy plik mat.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import datetime
+
 def aa():
     print("Moje pierwsze repo! Magda")
     print("Siema!")
+
+def dzis():
+    d = datetime.date.today()
+    today = d.strftime("%d/%m/%Y")
+    print(today)
+
 aa()
+dzis()
